1a5019f533a4651f3c66764092dd027a74b98adc
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-07-28 23:43:05 -0700

    Add files via upload

---
+++ funs_program.py
@@ -0,0 +1,43 @@
+# def greet(n):
+#     print('hello '+n)
+# greet('jaffar')
+# greet('munna')
+
+# def greet(*names):
+#     print('hello',names[3])
+# greet('car','is','my','wold') 
+
+# def details(n,a):
+#     print('jaffar '+n + '! your age is '+str(a))
+# details('jaffar',25)
+# details(n='jaffar',a=25)
+# details(a=26,n='shaik')
+
+# def details(n,a):
+#     print('hai '+n + '! your age is '+str(a))
+# n=input('enter your name: ')
+# a=input('enter yor age: ')
+# details(n,a)  
+
+
+
+
+# a={'1': [1,2],
+#    '2': ['niha','prava'],
+#    '3': ['AUG','FEB'],}
+
+# # print(a,type(a))       
+
+# import pandas as pd
+# df2=pd.DataFrame(a)
+# print(df2)
+
+# def r(x,y):
+#     return x+y
+# print(r(5,6))    
+
+# def z(p,q):
+#     return p*q,p+q;
+# p=int(input(10))
+# q=int(input(20))
+# print(z(p,q))
